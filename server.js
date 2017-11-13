c9140f64a722fc8878a7e783b8de57ce4d394ac0
davidocho <david@digitalguerrilla.com>
2017-11-13 15:45:57 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var http = require("http");
+var logger = require("morgan");
+var app = express();
+ 
+ 
+// Logging middleware
+app.use(logger("combined"));
+app.use(express.static('public')); 
+// set routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// Set server port
+var port = 4000;
+http.createServer(app).listen(port);
+console.log("Server is running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+
