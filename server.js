0d1266e610a03540bd9037f1ad65d4f7cf89f7cf
davidocho <david@digitalguerrilla.com>
2017-11-14 00:05:05 -0500

    fix for server errors

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var express = require("express");
-var http = require("http");
-var logger = require("morgan");
+var cors = require('cors');
 var app = express();
  
  
 // Logging middleware
-app.use(logger("combined"));
-app.use(express.static('public')); 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+app.use(express.static(__dirname + '/public')); 
+app.use(cors());
 // set routes
 app.get('/', function(req, res) {
   res.render('index');
 });
 
 // Set server port
-var port = 4040;
-http.createServer(app).listen(port);
+var port = process.env.PORT || 4050;
+app.listen(port);
 console.log("Server is running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
-
-
