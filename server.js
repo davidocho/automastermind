be919c6f059bc8901f4fbc6a6c78a38b788a2d27
davidocho <david@digitalguerrilla.com>
2017-11-13 23:56:21 -0500

    refactored server

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 var express = require("express");
-var cors = require('cors');
+var http = require("http");
+var logger = require("morgan");
 var app = express();
  
  
 // Logging middleware
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-});
-app.use(express.static(__dirname + '/public')); 
-app.use(cors());
+app.use(logger("combined"));
+app.use(express.static('public')); 
 // set routes
 app.get('/', function(req, res) {
   res.render('index');
 });
 
 // Set server port
-var port = process.env.PORT || 4040;
-app.listen(port);
+var port = 4040;
+http.createServer(app).listen(port);
 console.log("Server is running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
 
 
